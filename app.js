6611fd72df56b056bbb1a36fb2a94478f03d2df6
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-19 15:31:35 -0500

    Promesas: resolve y then para ok, reject y catch para error

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
 "use strict";
 //Función anónima autoinvocada
 (function () {
-    //Desestructuración de objetos
-    var avenger = {
-        nombre: 'Steve',
-        clave: 'Capitan América',
-        poder: 'Droga'
-    };
-    console.log(avenger.nombre);
-    console.log(avenger.clave);
-    console.log(avenger.poder);
-    //------------------//
-    var nombre = avenger.nombre, clave = avenger.clave, poder = avenger.poder;
-    console.log(nombre);
-    console.log(clave);
-    console.log(poder);
-    //------------------//
-    var extraer = function (_a) {
-        var nombre = _a.nombre, poder = _a.poder;
-        console.log(nombre);
-        console.log(poder);
-    };
-    extraer(avenger);
-    //------------------//
-    //Desestructuración de arreglos
-    var avengers = ['Thor', 'Ironman', 'Spiderman'];
-    console.log(avengers[0]);
-    console.log(avengers[1]);
-    console.log(avengers[2]);
-    var uno = avengers[0], dos = avengers[1], tres = avengers[2];
-    console.log(uno);
-    console.log(dos);
-    console.log(tres);
-    var extraerArray = function (_a) {
-        var thor = _a[0], ironman = _a[1], spiderman = _a[2];
-        console.log(thor);
-        console.log(ironman);
-        console.log(spiderman);
-    };
-    extraerArray(avengers);
+    //Promesas: resolve para ok, reject para error
+    console.log('Inicio');
+    const prom1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Se terminó el timeout');
+        }, 1000);
+    });
+    prom1
+        .then(mensaje => console.log(mensaje))
+        .catch(error => console.warn(error));
+    console.log('Fin');
 })();
