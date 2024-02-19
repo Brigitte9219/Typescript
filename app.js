47e54cc58d1ec47bd5e4e9db5761ec5222833cf6
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-19 16:27:11 -0500

    Clases y constructores

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 "use strict";
 //Función anónima autoinvocada
 (function () {
-    const enviarMision = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la misión`);
-    };
-    const regresarAlCuartel = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la misión`);
-    };
-    const wolverine = {
-        nombre: 'Logan',
-        edad: 60
-    };
-    enviarMision(wolverine);
-    regresarAlCuartel(wolverine);
+    //Clases y constructores
+    class Avenger {
+        //nombre:string='sin nombre';
+        //equipo:string;
+        //nombreReal:string;
+        //puedePelear:boolean;
+        //peleasGanadas:number;
+        constructor(nombre, equipo, nombreReal, puedePelear = true, peleasGanadas = 0) {
+            this.nombre = nombre;
+            this.equipo = equipo;
+            this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleasGanadas = peleasGanadas;
+        }
+    }
+    const antman = new Avenger('Antman', 'Capi');
+    console.log(antman);
 })();
