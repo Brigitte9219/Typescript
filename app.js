60a2cef7ae7ba99d918912fc5109ddec10b7790a
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-19 14:29:33 -0500

    Funciones de flecha

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 "use strict";
 //Función anónima autoinvocada
 (function () {
-    //Parámetros obligatorios, opcionales y por defecto
-    function activar(quien, momento, objeto = 'batiseñal') {
-        //console.log(`${quien} activó la ${objeto}`);
-        if (momento) {
-            console.log(`${quien} activo la ${objeto} en la ${momento}.`);
+    //Funciones de flecha
+    var miFuncion = function (a) {
+        return a;
+    };
+    //--------------------//
+    var miFuncionN = function (a) {
+        return a.toUpperCase();
+    };
+    var miFuncionF = function (a) { return a.toUpperCase(); };
+    console.log(miFuncionN('Normal'));
+    console.log(miFuncionF('Flecha'));
+    //--------------------//
+    var sumarN = function (a, b) {
+        return a + b;
+    };
+    var sumarF = function (a, b) { return a + b; };
+    console.log(sumarN(2, 3));
+    console.log(sumarF(3, 3));
+    //--------------------//
+    var hulk = {
+        nombre: 'Hulk',
+        smash: function () {
+            var _this = this;
+            setTimeout(function () {
+                console.log("".concat(_this.nombre, " smash!!!"));
+            }, 1000);
         }
-        else {
-            console.log(`${quien} activo la ${objeto}.`);
-        }
-    }
-    activar('Gordon', 'tarde');
+    };
+    hulk.smash();
 })();
